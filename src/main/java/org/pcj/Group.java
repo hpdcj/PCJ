a09945d64a818f502c640e8dbcacb85fcbd024cf
Marek Nowicki <faramir2@gmail.com>
2019-03-22 13:23:00 +0100

    Updating JavaDocs

--- src/main/java/org/pcj/Group.java
+++ src/main/java/org/pcj/Group.java
@@ -16,8 +16,7 @@ package org.pcj;
 public interface Group {
 
     /**
-     * Gets identifier of current PCJ Thread in the group. Identifiers are
-     * consecutive numbers that start with 0.
+     * Gets identifier of current PCJ Thread in the group. Identifiers are consecutive numbers that start with 0.
      *
      * @return current PCJ Thread identifier
      */
@@ -38,34 +37,31 @@ public interface Group {
     String getName();
 
     /**
-     * Starts asynchronos barrier. After starting barrier the PcjFuture is
-     * returned.
+     * Starts asynchronos barrier. After starting barrier the PcjFuture is returned.
      * <p>
-     * PCJ Thread can continue to work and can check returned PcjFuture if every
-     * thread done this barrier using {@link PcjFuture#isDone()} method.
+     * PCJ Thread can continue to work and can check returned PcjFuture to find if every thread arrive at this barrier
+     * using {@link PcjFuture#isDone()} method.
      * PcjFuture returns null when completed.
      *
-     * @return {@link org.pcj.PcjFuture} to check barrier state
+     * @return {@link org.pcj.PcjFuture}&lt;{@link java.lang.Void}&gt; to check barrier state
      */
     PcjFuture<Void> asyncBarrier();
 
     /**
-     * Starts asynchronous barrier with one peer PCJ Thread. Given threadId
-     * should be different from current PCJ Thread id, otherwise the exception
-     * is thrown.
+     * Starts asynchronous barrier with one peer PCJ Thread.
+     * Given threadId should be different from current PCJ Thread id, otherwise the exception is thrown.
      * <p>
-     * PCJ Thread can continue to work and can check returned PcjFuture if every
-     * thread done this barrier using {@link PcjFuture#isDone()} method.
+     * PCJ Thread can continue to work and can check returned PcjFuture to find if every thread done this barrier
+     * using {@link PcjFuture#isDone()} method.
      * PcjFuture returns null when completed.
      *
      * @param threadId current group PCJ Thread
-     * @return {@link org.pcj.PcjFuture} to check barrier state
+     * @return {@link org.pcj.PcjFuture}&lt;{@link java.lang.Void}&gt; to check barrier state
      */
     PcjFuture<Void> asyncBarrier(int threadId);
 
     /**
-     * Asynchronous get operation. Gets value of shared variable from PCJ Thread
-     * from the group.
+     * Asynchronous get operation. Gets value of shared variable from PCJ Thread from the group.
      *
      * @param <T>      type of value
      * @param threadId peer PCJ Thread
@@ -76,8 +72,7 @@ public interface Group {
     <T> PcjFuture<T> asyncGet(int threadId, Enum<?> variable, int... indices);
 
     /**
-     * Asynchronous collect operation. Gets value of shared variable from all PCJ Threads
-     * from the group.
+     * Asynchronous collect operation. Gets value of shared variable from all PCJ Threads from the group.
      *
      * @param <T>      type of value
      * @param variable variable name
@@ -87,8 +82,7 @@ public interface Group {
     <T> PcjFuture<T> asyncCollect(Enum<?> variable, int... indices);
 
     /**
-     * Asynchronous reduce operation. Reduce value of shared variable from all PCJ Threads
-     * from the group.
+     * Asynchronous reduce operation. Reduce value of shared variable from all PCJ Threads from the group.
      *
      * @param <T>      type of value
      * @param function reduce function
@@ -99,8 +93,8 @@ public interface Group {
     <T> PcjFuture<T> asyncReduce(ReduceOperation<T> function, Enum<?> variable, int... indices);
 
     /**
-     * Asynchronous put operation. Puts value into shared variable to PCJ Thread
-     * from the group.
+     * Asynchronous put operation. Puts value into shared variable to PCJ Thread from the group.
+     * PcjFuture returns null when completed.
      *
      * @param <T>      type of value
      * @param newValue new variable value
@@ -111,23 +105,33 @@ public interface Group {
      */
     <T> PcjFuture<Void> asyncPut(T newValue, int threadId, Enum<?> variable, int... indices);
 
+    /**
+     * Asynchronous accumulate operation. Accumulate value into shared variable to PCJ thread from the group.
+     * PcjFuture returns null when completed.
+     *
+     * @param <T>      type of value
+     * @param function reduce function
+     * @param newValue new variable value
+     * @param threadId peer PCJ Thread
+     * @param variable variable name
+     * @param indices  (optional) indices for array variable
+     * @return {@link org.pcj.PcjFuture}&lt;{@link java.lang.Void}&gt;
+     */
     <T> PcjFuture<Void> asyncAccumulate(ReduceOperation<T> function, T newValue, int threadId, Enum<?> variable, int... indices);
 
     /**
-     * Asynchronous execution operation. Executes associated function on
-     * specified thread and returns value.
+     * Asynchronous execution operation. Executes associated function on specified thread and returns value.
      *
      * @param <T>       type of returned value
      * @param threadId  peer PCJ Thread
      * @param asyncTask function to be executed
-     * @return {@link org.pcj.PcjFuture} that will contain value returned by the
-     * function
+     * @return {@link org.pcj.PcjFuture} that will contain value returned by the function
      */
     <T> PcjFuture<T> asyncAt(int threadId, AsyncTask<T> asyncTask);
 
     /**
-     * Asynchronous broadcast operation. Broadcasts value into shared variable
-     * of all PCJ Threads from the group.
+     * Asynchronous broadcast operation. Broadcasts value into shared variable of all PCJ Threads from the group.
+     * PcjFuture returns null when completed.
      *
      * @param <T>      type of value
      * @param newValue new variable value
--- src/main/java/org/pcj/PCJ.java
+++ src/main/java/org/pcj/PCJ.java
@@ -31,27 +31,24 @@ final public class PCJ extends InternalPCJ {
     /**
      * Starts PCJ calculations on local node using specified StartPoint.
      * NodesDescription contains list of all hostnames used in calculations.
-     * Hostnames can be specified many times, so more than one instance of PCJ
-     * will be run on node (called threads).
+     * Hostnames can be specified many times, so more than one instance of PCJ will be run on node (called threads).
      *
      * @param startPoint       start point class
      * @param nodesDescription description of used nodes (and threads)
      */
-    public static void start(Class<? extends StartPoint> startPoint, NodesDescription nodesDescription
-    ) {
+    public static void start(Class<? extends StartPoint> startPoint, NodesDescription nodesDescription) {
         InternalPCJ.start(startPoint, nodesDescription);
     }
 
     /**
-     * Deploys and starts PCJ calculations on nodes using specified StartPoint
-     * class. NodesDescription contains list of all hostnames used in
-     * calculations. Hostnames can be specified many times, so more than one
-     * instance of PCJ will be run on node (called threads). Empty hostnames
-     * means current JVM.
+     * Deploys and starts PCJ calculations on nodes using specified StartPoint class.
+     * NodesDescription contains list of all hostnames used in calculations.
+     * Hostnames can be specified many times, so more than one instance of PCJ will be run on node (called threads).
+     * Empty hostnames means current JVM.
      * <p>
-     * Hostnames can take port (after colon ':'), eg. ["localhost:8000",
-     * "localhost:8001", "localhost", "host2:8001", "host2"]. Default port is
-     * 8091 and can be modified using
+     * Hostnames can take port (after colon ':'),
+     * eg. ["localhost:8000", "localhost:8001", "localhost", "host2:8001", "host2"].
+     * Default port is 8091 and can be modified using
      * <tt>pcj.port</tt> system property value (-Dpcj.port=8091).
      *
      * @param startPoint       start point class
@@ -63,8 +60,7 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Gets unique node identifier. Node identifiers are consecutive numbers
-     * that starts from 0.
+     * Gets unique node identifier. Node identifiers are consecutive numbers that starts from 0.
      *
      * @return node identifier
      */
@@ -82,8 +78,8 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Gets identifier of current PCJ Thread in the global group. Identifiers
-     * are consecutive numbers that start with 0.
+     * Gets identifier of current PCJ Thread in the global group.
+     * Identifiers are consecutive numbers that start with 0.
      *
      * @return current PCJ Thread identifier
      */
@@ -103,8 +99,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Gets global group.
      * <p>
-     * Static methods of this class and methods of the global group do the same
-     * things -- static methods are wrapper for methods in global group.
+     * Static methods of this class and methods of the global group do the same things
+     * -- static methods are wrapper for methods in global group.
      *
      * @return global group
      */
@@ -116,8 +112,7 @@ final public class PCJ extends InternalPCJ {
      * Register storage as enum' constants.
      * <p>
      * This method creates object pointing by enum' annotation when necessary,
-     * that is when the class pointed by annotation was not previously used to
-     * store shared variables.
+     * that is when the class pointed by annotation was not previously used to store shared variables.
      *
      * @param storageEnumClass Enum class that represents storage shared
      *                         variables
@@ -134,11 +129,10 @@ final public class PCJ extends InternalPCJ {
     /**
      * Register storage as enum' constants.
      * <p>
-     * This method uses provided object as storage of shared variables. The
-     * object has to be the same type as the type pointed by enum' annotation.
+     * This method uses provided object as storage of shared variables.
+     * The object has to be the same type as the type pointed by enum' annotation.
      * <p>
-     * If the {@code storageObject} is {@code null}, it works like
-     * {@link #registerStorage(java.lang.Class)} method.
+     * If the {@code storageObject} is {@code null}, it works like {@link #registerStorage(java.lang.Class)} method.
      *
      * @param storageEnumClass Enum class that represents storage shared
      *                         variables
@@ -166,11 +160,10 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Starts asynchronos barrier. After starting barrier the PcjFuture is
-     * returned.
+     * Starts asynchronos barrier. After starting barrier the PcjFuture is returned.
      * <p>
-     * PCJ Thread can continue to work and can check returned PcjFuture if every
-     * thread done this barrier using {@link PcjFuture#isDone()} method.
+     * PCJ Thread can continue to work and can check returned PcjFuture to find if every thread arrive at this barrier
+     * using {@link PcjFuture#isDone()} method.
      * PcjFuture returns null when completed.
      *
      * @return PcjFuture to check barrier state
@@ -191,12 +184,11 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Starts asynchronous barrier with one peer PCJ Thread. Given threadId
-     * should be different from current PCJ Thread id, otherwise the exception
-     * is thrown.
+     * Starts asynchronous barrier with one peer PCJ Thread.
+     * Given threadId should be different from current PCJ Thread id, otherwise the exception is thrown.
      * <p>
-     * PCJ Thread can continue to work and can check returned PcjFuture if every
-     * thread done this barrier using {@link PcjFuture#isDone()} method.
+     * PCJ Thread can continue to work and can check returned PcjFuture to find if every thread arrive at this barrier
+     * using {@link PcjFuture#isDone()} method.
      * PcjFuture returns null when completed.
      *
      * @param threadId global PCJ Thread id
@@ -209,7 +201,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous barrier with one peer PCJ Thread.
      * <p>
-     * Wrapper for {@link #asyncBarrier(int)}. It is the equivalent to call:
+     * Wrapper for {@link #asyncBarrier(int)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.asyncBarrier(threadId).get();}</blockquote>
      *
@@ -230,8 +223,12 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Checks and optionally waits for one modification. Decrease number of
-     * modification count.
+     * Checks and optionally waits for one modification.
+     * Decrease number of modification count.
+     * <p>
+     * It is the equivalent to call:
+     *
+     * <blockquote>{@code PCJ.waitFor(variable, 1);}</blockquote>
      *
      * @param variable shared variable
      * @return remaining modification count
@@ -242,7 +239,7 @@ final public class PCJ extends InternalPCJ {
 
     /**
      * Checks and optionally waits for many ({@code count}) modifications.
-     * Decrease number of modification count.
+     * Decreases number of modification count.
      *
      * @param variable shared variable
      * @param count    number of modifications
@@ -254,7 +251,7 @@ final public class PCJ extends InternalPCJ {
 
     /**
      * Checks and optionally waits for many ({@code count}) modifications.
-     * Decrease number of modification count.
+     * Decreases number of modification count.
      *
      * @param variable shared variable
      * @param count    number of modifications
@@ -300,8 +297,26 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Asynchronous get operation. Gets value of shared variable from PCJ Thread
-     * from the global group.
+     * Accumulates value to shared variable of current PCJ Thread.
+     *
+     * @param <T>      type of variable
+     * @param function reduce function
+     * @param newValue value (reference)
+     * @param variable variable name
+     * @param indices  (optional) indices for array variable
+     * @throws PcjRuntimeException contains wrapped exception (eg.
+     *                             ArrayOutOfBoundException).
+     */
+    public static <T> void accumulateLocal(ReduceOperation<T> function, T newValue, Enum<?> variable, int... indices) throws PcjRuntimeException {
+        try {
+            PcjThread.getCurrentThreadData().getStorages().accumulate(function, newValue, variable, indices);
+        } catch (Exception ex) {
+            throw new PcjRuntimeException(ex);
+        }
+    }
+
+    /**
+     * Asynchronous get operation. Gets value of shared variable from PCJ Thread from the global group.
      *
      * @param <T>      type of value
      * @param threadId peer PCJ Thread
@@ -316,8 +331,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous get operation.
      * <p>
-     * Wrapper for {@code asyncGet(int, java.lang.Enum<?>, int...)}. It is the
-     * equivalent to call:
+     * Wrapper for {@code asyncGet(int, java.lang.Enum<?>, int...)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncGet(threadId, variable, indices).get();}</blockquote>
      *
@@ -334,8 +349,7 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Asynchronous collect operation. Gets value of shared variable from all PCJ Threads
-     * from the group.
+     * Asynchronous collect operation. Gets value of shared variable from all PCJ Threads from the group.
      *
      * @param <T>      type of value
      * @param variable variable name
@@ -349,8 +363,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous collect operation.
      * <p>
-     * Wrapper for {@code asyncCollect(java.lang.Enum<?>, int...)}. It is
-     * the equivalent to call:
+     * Wrapper for {@code asyncCollect(java.lang.Enum<?>, int...)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncCollect(variable, indices).get();}</blockquote>
      *
@@ -364,8 +378,7 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Asynchronous reduce operation. Reduce value of shared variable from all PCJ Threads
-     * from the group.
+     * Asynchronous reduce operation. Reduce value of shared variable from all PCJ Threads from the group.
      *
      * @param <T>      type of value
      * @param function reduce function
@@ -380,8 +393,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous reduce operation.
      * <p>
-     * Wrapper for {@code asyncReduce(BinaryOperator<T>, java.lang.Enum<?>, int...)}. It is
-     * the equivalent to call:
+     * Wrapper for {@code asyncReduce(BinaryOperator<T>, java.lang.Enum<?>, int...)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncReduce(function, variable, indices).get();}</blockquote>
      *
@@ -392,12 +405,11 @@ final public class PCJ extends InternalPCJ {
      * @return {@link org.pcj.PcjFuture} that will contain shared variable value
      */
     public static <T> T reduce(ReduceOperation<T> function, Enum<?> variable, int... indices) {
-        return PCJ.<T>asyncReduce(function, variable, indices).get();
+        return PCJ.asyncReduce(function, variable, indices).get();
     }
 
     /**
-     * Asynchronous put operation. Puts value into shared variable to PCJ Thread
-     * from the global group.
+     * Asynchronous put operation. Puts value into shared variable to PCJ Thread from the global group.
      *
      * @param <T>      type of value
      * @param newValue new variable value
@@ -413,8 +425,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous put operation.
      * <p>
-     * Wrapper for {@code asyncPut(T, int, java.lang.Enum<?>, int...)}. It is
-     * the equivalent to call:
+     * Wrapper for {@code asyncPut(T, int, java.lang.Enum<?>, int...)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncPut(newValue, threadId, variable, indices).get();}</blockquote>
      *
@@ -427,21 +439,47 @@ final public class PCJ extends InternalPCJ {
      *                             ArrayOutOfBoundException).
      */
     public static <T> void put(T newValue, int threadId, Enum<?> variable, int... indices) throws PcjRuntimeException {
-        PCJ.<T>asyncPut(newValue, threadId, variable, indices).get();
-    }
-
-    public static <T> PcjFuture<Void> asyncAccumulate(ReduceOperation<T> function,T newValue, int threadId, Enum<?> variable, int... indices) {
-        return getGlobalGroup().asyncAccumulate(function, newValue, threadId, variable, indices);
-    }
-
-
-    public static <T> void accumulate(ReduceOperation<T> function, T newValue, int threadId, Enum<?> variable, int... indices) throws PcjRuntimeException {
-        PCJ.<T>asyncAccumulate(function, newValue, threadId, variable, indices).get();
+        PCJ.asyncPut(newValue, threadId, variable, indices).get();
     }
 
     /**
-     * Asynchronous broadcast operation. Broadcasts value into shared variable
-     * of all PCJ Threads from the global group.
+     * Asynchronous accumulate operation. Accumulate value into shared variable to PCJ thread from the global group.
+     *
+     * @param <T>      type of value
+     * @param function reduce function
+     * @param newValue new variable value
+     * @param threadId peer PCJ Thread
+     * @param variable variable name
+     * @param indices  (optional) indices for array variable
+     * @return {@link org.pcj.PcjFuture}&lt;{@link java.lang.Void}&gt;
+     */
+    public static <T> PcjFuture<Void> asyncAccumulate(ReduceOperation<T> function, T newValue, int threadId, Enum<?> variable, int... indices) {
+        return getGlobalGroup().asyncAccumulate(function, newValue, threadId, variable, indices);
+    }
+
+    /**
+     * Synchronous accumulate operation.
+     * <p>
+     * Wrapper for {@code asyncAccumulate(ReduceOperation<T>, T, int, java.lang.Enum<?>, int...)}.
+     * It is the equivalent to call:
+     *
+     * <blockquote>{@code PCJ.<T>asyncAccumulate(function, newValue, threadId, variable, indices).get();}</blockquote>
+     *
+     * @param <T>      type of value
+     * @param function reduce function
+     * @param newValue new variable value
+     * @param threadId peer PCJ Thread
+     * @param variable variable name
+     * @param indices  (optional) indices for array variable
+     * @throws PcjRuntimeException contains wrapped exception (eg.
+     *                             ArrayOutOfBoundException).
+     */
+    public static <T> void accumulate(ReduceOperation<T> function, T newValue, int threadId, Enum<?> variable, int... indices) throws PcjRuntimeException {
+        PCJ.asyncAccumulate(function, newValue, threadId, variable, indices).get();
+    }
+
+    /**
+     * Asynchronous broadcast operation. Broadcasts value into shared variable of all PCJ Threads from the global group.
      *
      * @param <T>      type of value
      * @param newValue new variable value
@@ -456,8 +494,8 @@ final public class PCJ extends InternalPCJ {
     /**
      * Synchronous broadcast operation.
      * <p>
-     * Wrapper for {@code asyncBroadcast(T, Enum<?>)}. It is the equivalent to
-     * call:
+     * Wrapper for {@code asyncBroadcast(T, Enum<?>)}.
+     * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncBroadcast(variable, newValue).get();}</blockquote>
      *
@@ -467,12 +505,11 @@ final public class PCJ extends InternalPCJ {
      * @param indices  (optional) indices for array variable
      */
     public static <T> void broadcast(T newValue, Enum<?> variable, int... indices) {
-        PCJ.<T>asyncBroadcast(newValue, variable, indices).get();
+        PCJ.asyncBroadcast(newValue, variable, indices).get();
     }
 
     /**
-     * Asynchronous execution operation. Executes associated function on
-     * specified thread from global group and returns value.
+     * Asynchronous execution operation. Executes associated function on specified thread from global group and returns value.
      *
      * @param <T>       type of returned value
      * @param threadId  peer PCJ Thread
@@ -485,9 +522,9 @@ final public class PCJ extends InternalPCJ {
     }
 
     /**
-     * Asynchronous execution operation. Executes associated function on
-     * specified thread from global group and returns value.
+     * Asynchronous execution operation. Executes associated function on specified thread from global group and returns value.
      * <p>
+     * Wrapper for {@code asyncAt(int, AsyncTask<T>)}.
      * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncAt(threadId, asyncTask).get();}</blockquote>
@@ -499,26 +536,24 @@ final public class PCJ extends InternalPCJ {
      * function
      */
     public static <T> T at(int threadId, AsyncTask<T> asyncTask) throws PcjRuntimeException {
-        return PCJ.<T>asyncAt(threadId, asyncTask).get();
+        return PCJ.asyncAt(threadId, asyncTask).get();
     }
 
     /**
-     * Asynchronous execution operation. Executes associated function on
-     * specified thread from global group without returing value.
+     * Asynchronous execution operation. Executes associated function on specified thread from global group without returing value.
      *
      * @param threadId  peer PCJ Thread
      * @param asyncTask function to be executed
-     * @return {@link org.pcj.PcjFuture} that indicates finish execution of
-     * execution
+     * @return {@link org.pcj.PcjFuture} that indicates finish execution of execution
      */
     public static PcjFuture<Void> asyncAt(int threadId, AsyncTask.VoidTask asyncTask) throws PcjRuntimeException {
         return getGlobalGroup().asyncAt(threadId, asyncTask);
     }
 
     /**
-     * Asynchronous execution operation. Executes associated function on
-     * specified thread from global group without returing value.
+     * Asynchronous execution operation. Executes associated function on specified thread from global group without returing value.
      * <p>
+     * Wrapper for {@code asyncAt(int, AsyncTask.VoidTask)}.
      * It is the equivalent to call:
      *
      * <blockquote>{@code PCJ.<T>asyncAt(threadId, asyncTask).get();}</blockquote>
